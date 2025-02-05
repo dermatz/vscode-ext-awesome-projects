6f9f2ca0587ee98dfbbab5daac172e312909bd85
Mathias Elle <m.elle@e3n.de>
2025-02-05 21:43:51 +0100

    Feature: update button text and improve styling in project info dropdown and webview

--- src/template/project/components/dropdowns/dropdownProjectInfo.ts
+++ src/template/project/components/dropdowns/dropdownProjectInfo.ts
@@ -105,7 +105,7 @@ export async function getProjectInfoDropdownHtml(project: Project, color?: strin
                     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"/>
                     </svg>
-                    Show in File Manager
+                    Open Folder
                 </button>
             </div>
         </div>
