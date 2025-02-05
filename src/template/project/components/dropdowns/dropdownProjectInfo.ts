39be70efa42b44d4cea30be37849b62b291e32af
Mathias Elle <m.elle@e3n.de>
2025-02-05 21:46:31 +0100

    Feature: simplify project URL links in project info dropdown by removing redundant href attributes

--- src/template/project/components/dropdowns/dropdownProjectInfo.ts
+++ src/template/project/components/dropdowns/dropdownProjectInfo.ts
@@ -32,10 +32,7 @@ export async function getProjectInfoDropdownHtml(project: Project, color?: strin
                     ${
                         project.productionUrl
                             ? `
-                        <a href="${project.productionUrl.replace(
-                            /'/g,
-                            "\\'"
-                        )}" class="project-url" onclick="openUrl(event, '${project.productionUrl.replace(/'/g, "\\'")}')">
+                        <a class="project-url" onclick="openUrl(event, '${project.productionUrl.replace(/'/g, "\\'")}')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9-3-9m-9 9a9 9 0 019-9"/>
                             </svg>
@@ -47,10 +44,7 @@ export async function getProjectInfoDropdownHtml(project: Project, color?: strin
                     ${
                         project.stagingUrl
                             ? `
-                        <a href="${project.stagingUrl.replace(/'/g, "\\'")}" class="project-url" onclick="openUrl(event, '${project.stagingUrl.replace(
-                                    /'/g,
-                                    "\\'"
-                                )}')">
+                        <a class="project-url" onclick="openUrl(event, '${project.stagingUrl.replace(/'/g, "\\'")}')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                             </svg>
@@ -62,10 +56,7 @@ export async function getProjectInfoDropdownHtml(project: Project, color?: strin
                     ${
                         project.devUrl
                             ? `
-                        <a href="${project.devUrl.replace(/'/g, "\\'")}" class="project-url" onclick="openUrl(event, '${project.devUrl.replace(
-                                    /'/g,
-                                    "\\'"
-                                )}')">
+                        <a class="project-url" onclick="openUrl(event, '${project.devUrl.replace(/'/g, "\\'")}')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                             </svg>
@@ -77,10 +68,7 @@ export async function getProjectInfoDropdownHtml(project: Project, color?: strin
                     ${
                         project.managementUrl
                             ? `
-                        <a href="${project.managementUrl.replace(
-                            /'/g,
-                            "\\'"
-                        )}" class="project-url" onclick="openUrl(event, '${project.managementUrl.replace(/'/g, "\\'")}')">
+                        <a class="project-url" onclick="openUrl(event, '${project.managementUrl.replace(/'/g, "\\'")}')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"/>
                             </svg>
