b4006af4fcbf03e6eaf2825ef9c67613d0935b0d
Mathias Elle <m.elle@e3n.de>
2025-02-05 07:53:07 +0100

    Feature: add project info dropdown component for enhanced project details display

--- src/webviewProvider.ts
+++ src/webviewProvider.ts
@@ -6,6 +6,7 @@ import { generateGradient, getContrastColor } from './template/project/component
 import { getHeaderHtml } from './template/webview/header';
 import { getFooterHtml } from './template/webview/footer';
 import { getProjectListHtml } from './template/project/projectlist';
+import { getProjectInfoDropdownHtml } from './template/project/components/info-dropdown';
 
 /**
  * Project Components
@@ -270,6 +271,7 @@ export class ProjectsWebviewProvider implements vscode.WebviewViewProvider {
             const faviconHtml = baseUrl && useFavicons ? `<img src="https://www.google.com/s2/favicons?domain=${baseUrl}" onerror="this.parentElement.innerHTML='📁'">` : "📁";
 
             const projectSettingsHtml = await getSettingsDropdownHtml(this._context, project);
+            const projectInfoHtml = await getProjectInfoDropdownHtml(project);
 
             return `
                 <div class="project-item-wrapper" draggable="true" data-index="${index}">
@@ -287,99 +289,8 @@ export class ProjectsWebviewProvider implements vscode.WebviewViewProvider {
                             </svg>
                         </div>
                     </div>
-                    <div class="project-info-dropdown" id="info-${project.path.replace(/[^a-zA-Z0-9]/g, "-")}">
-                        <div class="info-section">
-                            <div class="info-label">Path</div>
-                            <div class="info-value">${project.path}</div>
-                        </div>
-                        ${ project.productionUrl || project.devUrl || project.stagingUrl || project.managementUrl
-                                ? `
-                        <div class="info-section">
-                            <div class="info-label">URLs</div>
-                            <div class="info-value">
-                                ${
-                                    project.productionUrl
-                                        ? `
-                                    <a href="${project.productionUrl.replace(
-                                        /'/g,
-                                        "\\'"
-                                    )}" class="project-url" onclick="openUrl(event, '${project.productionUrl.replace(/'/g, "\\'")}')">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9-3-9m-9 9a9 9 0 019-9"/>
-                                        </svg>
-                                        Production
-                                    </a>
-                                `
-                                        : ""
-                                }
-                                ${
-                                    project.stagingUrl
-                                        ? `
-                                    <a href="${project.stagingUrl.replace(/'/g, "\\'")}" class="project-url" onclick="openUrl(event, '${project.stagingUrl.replace(
-                                                /'/g,
-                                                "\\'"
-                                            )}')">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
-                                        </svg>
-                                        Staging
-                                    </a>
-                                `
-                                        : ""
-                                }
-                                ${
-                                    project.devUrl
-                                        ? `
-                                    <a href="${project.devUrl.replace(/'/g, "\\'")}" class="project-url" onclick="openUrl(event, '${project.devUrl.replace(
-                                                /'/g,
-                                                "\\'"
-                                            )}')">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                                        </svg>
-                                        Development
-                                    </a>
-                                `
-                                        : ""
-                                }
-                                ${
-                                    project.managementUrl
-                                        ? `
-                                    <a href="${project.managementUrl.replace(
-                                        /'/g,
-                                        "\\'"
-                                    )}" class="project-url" onclick="openUrl(event, '${project.managementUrl.replace(/'/g, "\\'")}')">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"/>
-                                        </svg>
-                                        Management (Jira, Trello, etc.)
-                                    </a>
-                                `
-                                        : ""
-                                }
-                            </div>
-                        </div>
-                        `
-                                : ""
-                        }
-                        <div class="info-actions">
-                            <button class="button info-action" onclick="openProject('${project.path.replace(/'/g, "\\'")}')">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3l14 9-14 9V3z"/>
-                                </svg>
-                                Open Project
-                            </button>
-                            <button class="button show-in-file-manager" data-path="${project.path.replace(/'/g, "\\'")}" data-name="${project.name}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"/>
-                                </svg>
-                                Show in File Manager
-                            </button>
-                        </div>
-                    </div>
-
+                    ${projectInfoHtml}
                     ${projectSettingsHtml}
-
                 </div>
             `;
             }));
