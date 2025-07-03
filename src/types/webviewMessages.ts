dc987ba040ac8f95a812ac07c645cdf20f445c76
Mathias Elle <m.elle@e3n.de>
2025-07-03 08:12:28 +0200

    refactor: remove drag and drop functionality and related styles for project items

--- src/types/webviewMessages.ts
+++ src/types/webviewMessages.ts
@@ -3,14 +3,12 @@ import { Project } from '../extension';
 export interface WebviewMessage {
     command: 'deleteProject' | 'updateProject' | 'openProject' | 'openUrl' |
              'addProject' | 'projectSelected' |
-             'reorderProjects' | 'scanProjects' | 'setLoading';
+             'scanProjects' | 'setLoading';
     projectId?: string;
     projectPath?: string;
     project?: Project;
     url?: string;
     updates?: Partial<Project>;
     path?: string;
-    oldIndex?: number;
-    newIndex?: number;
     isLoading?: boolean;
 }
